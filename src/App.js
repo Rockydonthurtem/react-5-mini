eb43e9009075dcf745f5446185b92a77809e5bcd
Rockydonthurtem <charrisq@hotmail.com>
2018-07-09 08:00:45 -0500

    mini

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux';
+import increment from './ducks/counter';
+import decrement from './ducks/counter'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    const { currentValue, decrement, increment } = this.props;
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
+          <h1 className="counter__current-value">{0}</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={ () => null }
+              onClick={() => this.props.incrment}
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={ () => null }
+              onClick={() => increment(5)}
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={ () => null }
+              onClick={() => decrement(1)}
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={ () => null }
+              onClick={() => (5)}
             >
               -5
             </button>
             <br />
             <button
               className="counter__button undo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={true}
+              onClick={() => null}
             >
               Undo
             </button>
             <button
               className="counter__button redo"
-              disabled={ true }
-              onClick={ () => null }
+              disabled={true}
+              onClick={() => null}
             >
               Redo
             </button>
@@ -51,7 +56,7 @@ class App extends Component {
         </section>
         <section className="state">
           <pre>
-            { JSON.stringify( this.props, null, 2 ) }
+            {JSON.stringify(this.props, null, 2)}
           </pre>
         </section>
       </div>
@@ -59,4 +64,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connects(mapDispatchToProps, { increment, decrement })(App);
